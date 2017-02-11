ce67e886534e4848da28daa69a1344d9e374c648
niuyongwang <1992685287@qq.com>
2017-02-11 19:41:41 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 
 int main(int argc, const char* argv[])
 {
