b628f758d8ce0dab03acff643fb7f12abe17a340
itheima <1992685287@qq.com>
2017-02-11 18:31:57 +0800

    hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, const char* argv[])
+{
+    printf("hello world\n");
+    return 0;
+}
